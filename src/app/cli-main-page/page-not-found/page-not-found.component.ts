5137652fbb106880e532d0d74ff8ef9422665e85
georgy.saukov <gsaukov@yahoo.com>
2021-09-15 18:50:43 +0200

    page not found review

--- src/app/cli-main-page/page-not-found/page-not-found.component.ts
+++ src/app/cli-main-page/page-not-found/page-not-found.component.ts
@@ -2,29 +2,30 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+    selector: 'app-page-not-found',
+    templateUrl: './page-not-found.component.html',
+    styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent implements OnInit, AfterViewInit  {
+export class PageNotFoundComponent implements OnInit, AfterViewInit {
 
-  counter:number = 1000
-  interval:number = 10
+    counter: number = 1000;
+    interval: number = 10;
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit(): void {
-    let loader = document.getElementById('loader')
-    loader.style.display = "none";
-  }
-
-  tryRedirect():boolean {
-    if (this.counter === 0) {
-      this.router.navigate(['/'])
+    constructor(private router: Router) {
     }
-    return true;
-  }
+
+    ngOnInit(): void {
+    }
+
+    ngAfterViewInit(): void {
+        let loader = document.getElementById('loader');
+        loader.style.display = 'none';
+    }
+
+    tryRedirect(): boolean {
+        if (this.counter === 0) {
+            this.router.navigate(['/']);
+        }
+        return true;
+    }
 }
